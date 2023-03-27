d642a051cf3e64172beaab5a62c1d2d403b836f5
Ragimov Fuad <foyado@gmail.com>
2023-03-27 23:59:12 +0300

    finished dz3. done all tasks

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,8 @@
-function filterRange(arr, a, b) {
-  // ваш код...
+function filterRange(arr, a = 0, b = 0) {
+  if (a === 0 && b === 0) {
+    return arr
+  } else {
+    return arr.filter(char => char <= Math.max(a, b) && char >= Math.min(a, b))
+  }
 }
+
