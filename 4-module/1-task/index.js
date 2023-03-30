1c05c266c7833dfe409376022b856e9ddc435de7
Ragimov Fuad <foyado@gmail.com>
2023-03-30 17:29:18 +0300

    finished dz4. done all tasks

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement("ul");
+  friends.forEach(el => {
+    let li = document.createElement("li");
+    li.innerText = `${el.firstName} ${el.lastName}`
+    ul.append(li)
+  })
+  return ul
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,28 @@
 function highlight(table) {
-  // ваш код...
+  let list = table.querySelector('tbody');
+  let rows = list.querySelectorAll("tr");
+
+  rows.forEach(el => {
+    if(!el.cells[3].hasAttribute('data-available')) {
+      el.hidden = true
+    } else if (el.cells[3].dataset.available === "true") {
+      el.classList.add("available");
+    } else {
+      el.classList.add("unavailable");
+    }
+
+    switch (el.cells[2].textContent) {
+      case "m":
+        el.classList.add("male")
+        break
+      case "f":
+        el.classList.add("female")
+        break
+    }
+
+    if (+el.cells[1].textContent < 18) {
+      el.style.textDecoration = "line-through"
+    }
+
+  });
 }
